9d12329d4568646810e3a0a627497270d17496fe
chanjet <1209829998@qq.com>
2020-01-02 09:49:11 +0800

    zuul

---
+++ netflix-zuul-core/src/main/java/com/netflix/zuul/ExecutionStatus.java
@@ -0,0 +1,17 @@
+package com.netflix.zuul;
+
+public enum ExecutionStatus {
+
+	SUCCESS(1), SKIPPED(-1), DISABLED(-2), FAILED(-3);
+
+	private int status;
+
+	ExecutionStatus(int status) {
+		this.status = status;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+	
+}
